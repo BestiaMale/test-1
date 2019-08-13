152db780ad08ad99853bc5f2090427a8cbd97963
BeastHQ <Bestia@users.noreply.github.com>
2019-08-13 23:18:12 +0500

    Starting to make BlackJack game

--- main.rb
+++ main.rb
@@ -1,2 +1,9 @@
-puts "Hello world!"
-return 0
+def check(chand,phand)
+  chand = chand.each
+end
+pull = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+croupier = [pull[rand(12)]]
+player = [pull[rand(12)],pull[rand(12)]]
+loop{
+
+}
