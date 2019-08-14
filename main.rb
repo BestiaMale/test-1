ddf2c3e702f7f94a5b008ee3b8d8c412a2e02664
BeastHQ <Bestia@users.noreply.github.com>
2019-08-14 17:12:59 +0500

    New file 'check.rb'

--- main.rb
+++ main.rb
@@ -1,9 +1,12 @@
 def check(chand,phand)
-  chand = chand.each
+  csum = psum = 0
+  ['J','Q','K'].each{|x|
+    csum+=chand.count(x)
+    psum+=phand.count(x)
+    print csum+" "+psum
+  }
 end
 pull = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 croupier = [pull[rand(12)]]
 player = [pull[rand(12)],pull[rand(12)]]
-loop{
-
-}
+check(croupier,player)
