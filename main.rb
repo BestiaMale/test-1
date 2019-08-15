a50f259e018a7a29a31bcbcb6650f83867857dcf
BeastHQ <Bestia@users.noreply.github.com>
2019-08-15 19:51:40 +0500

    Fixed mistypes

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-require 'check'
+require 'CheckHand.rb'
 pull = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
-puts croupier = [pull[rand(12)]
+puts croupier = [pull[rand(12)]]
 player = [pull[rand(12)], pull[rand(12)]]
 check(croupier, player)
