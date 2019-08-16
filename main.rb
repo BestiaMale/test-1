22bfba4ea1a6d5035a3f63446faa3361db219cde
BeastHQ <Bestia@users.noreply.github.com>
2019-08-16 19:44:07 +0500

    Deleted check

--- main.rb
+++ main.rb
@@ -1,5 +1,39 @@
-require 'CheckHand.rb'
+def check(chand,phand)
+  csum = psum = 0
+  chand.each { |x|
+    case x
+    when 'J'
+      csum += 2
+    when 'Q'
+      csum += 3
+    when 'K'
+      csum += 4
+    when 'A'
+      csum += csum>10 ? 1 : 11
+    else
+      csum += x
+    end
+    }
+  phand.each { |x|
+    case x
+    when 'J'
+      psum += 2
+    when 'Q'
+      psum += 3
+    when 'K'
+      psum += 4
+    when 'A'
+      psum += psum>10 ? 1 : 11
+    else
+      psum += x
+    end
+    }
+  ans = "#{csum} #{psum}"
+  return ans
+end
 pull = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
-puts croupier = [pull[rand(12)]]
-player = [pull[rand(12)], pull[rand(12)]]
-check(croupier, player)
+croupier_hand = [pull[rand(12)]]
+player_hand = [pull[rand(12)], pull[rand(12)]]
+croupier_score, player_score = check(croupier_hand, player_hand).split()
+puts [croupier_hand, player_hand].join(' ')
+puts [croupier_score, player_score].join(' ')
