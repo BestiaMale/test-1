e4fe483b18edc7031ea39cee8426da83107559dd
BeastHQ <Bestia@users.noreply.github.com>
2019-08-17 22:09:03 +0500

    Looped the game

--- main.rb
+++ main.rb
@@ -17,12 +17,19 @@ def count(hand)
     return sum
 end
 pull = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+loop{
+puts "To start a new game enter 1, else nothing"
+if gets.to_i != 1
+  puts "Game closed."
+  STDIN.getc
+  exit
+end
 croupier = [pull[rand(12)]]
 player = [pull[rand(12)], pull[rand(12)]]
 if count(player)==21
   puts "Your hand: #{player.join(' ')} (#{count(player)})\nYou win!"
   STDIN.getc
-  exit
+  break
 end
   puts "Your hand: #{player.join(' ')} (#{count(player)})\nIf you want to continue, enter 1, else nothing"
 loop{
@@ -32,11 +39,11 @@ loop{
     if count(player) == 21
       puts "BlackJack! You win!"
       STDIN.getc
-      exit
+      break
     elsif count(player) > 21
       puts "Overdraft! You lose!"
       STDIN.getc
-      exit
+      break
     else
       puts "Your hand: #{player.join(' ')} (#{count(player)})\nIf you want to continue, enter 1, else nothing"
     end
@@ -44,28 +51,31 @@ loop{
     break
   end
 }
-puts "Your score: #{count(player)}"
-loop{
-  puts "Cropier took a card"
-  croupier.push(pull[rand(12)])
-  puts "Croupier hand: #{croupier.join(' ')} (#{count(croupier)})"
-  if count(croupier) == 21
-    puts "BlackJack! Cropier win!"
-    STDIN.getc
-    exit
-  elsif count(croupier) > 21
-    puts "Overdraft! You win!"
-    STDIN.getc
-    exit
-  elsif count(croupier) > 16
-    if count(croupier)>= count(player)
-      puts "Your score: #{count(player)}\nCroupier score: #{count(croupier)}\nCroupier win!"
+if count(player) < 21
+  puts "Your score: #{count(player)}"
+  loop{
+    puts "Cropier took a card"
+    croupier.push(pull[rand(12)])
+    puts "Croupier hand: #{croupier.join(' ')} (#{count(croupier)})"
+    if count(croupier) == 21
+      puts "BlackJack! Cropier win!"
       STDIN.getc
-      exit
-    else
-      puts "Your score: #{count(player)}\nCroupier score: #{count(croupier)}\nYou win!"
+      break
+    elsif count(croupier) > 21
+      puts "Overdraft! You win!"
       STDIN.getc
-      exit
+      break
+    elsif count(croupier) > 16
+      if count(croupier)>= count(player)
+        puts "Your score: #{count(player)}\nCroupier score: #{count(croupier)}\nCroupier win!"
+        STDIN.getc
+        break
+      else
+        puts "Your score: #{count(player)}\nCroupier score: #{count(croupier)}\nYou win!"
+        STDIN.getc
+        break
+      end
     end
-  end
+  }
+end
 }
